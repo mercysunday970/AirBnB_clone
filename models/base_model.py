097a05b3f505c8b763b71de13ccf4bd7f59a0942
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-02-17 15:41:15 -0800

    Console

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,18 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-	""" This is Base Class from which other subclasses
-	will inherit"""
-	def __init__(self):
+    """ This is Base Class from which other subclasses
+    will inherit"""
+    def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}"\
+            .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
