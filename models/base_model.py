5a62f46fbd8499761252ac59165486b21ac13556
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-02-18 00:32:00 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class BaseModel:
 	""" This is Base Class from which other subclasses
 	will inherit"""
-    def __init__(self):
+	def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
