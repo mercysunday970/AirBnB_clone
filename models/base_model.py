44e137c14ea6fa8ac460befaf2f906f79c52b7d6
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-02-18 01:08:49 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,17 @@ from datetime import datetime
 class BaseModel:
     """ This is Base Class from which other subclasses
     will inherit"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}"\
