9d97d810e6a9dc2309be5ba0c608f3f890c933f9
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-02-18 01:11:41 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -11,8 +11,8 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.\
-                        strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    setattr(self, key, datetime\
+                        .strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
                 else:
                     setattr(self, key, value)
         else:
