d42e9e4033c4616cf35c6f0d344bfe60c8e7843c
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-02-19 16:06:37 +0100

    console

--- console.py
+++ console.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+"""Defines the HBnB console."""
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """Defines the HolbertonBnB command interpreter.
+
+    Attributes:
+        prompt (str): The command prompt.
+    """
+    
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
@@ -15,6 +24,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty line"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
