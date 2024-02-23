4048e29c8860a4c1f6ca0096e720c0e62f572108
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-02-23 12:36:51 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,28 +9,14 @@ class BaseModel:
     """ This is Base Class from which other subclasses
     will inherit"""
     def __init__(self, *args, **kwargs):
-        if kwargs:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-                if key == "created_at":
-                    self.created_at = datetime.strptime(value,
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                if key == "updated_at":
-                    self.updated_at = datetime.strptime(value,
-                                                        "%Y-%m-%dT%H:%M:%S.%f")
-                if "id" not in kwargs:
-                    self.id = str(uuid.uuid4())
-                if "created_at" not in kwargs:
-                    self.created_at = datetime.now()
-                if "updated_at" not in kwargs:
-                    self.created_at = datetime.now()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-        models.storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}"\
