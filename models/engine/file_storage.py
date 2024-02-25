b0adc124fb5bf6d1390ea5f25d71f3d745d2649b
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-02-25 13:58:47 +0100

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -36,8 +36,15 @@ class FileStorage:
         """ Deserializes __objects from the JSON file """
         from models.base_model import BaseModel
         from models.user import User
+        from models.place import Place
+        from models.city import City
+        from models.amenity import Amenity
+        from models.state import State
+        from models.review import Review
 
-        d__ct = {'BaseModel': BaseModel, 'User': User}
+        d__ct = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
+               'City': City, 'Amenity': Amenity, 'State': State,
+               'Review': Review}
 
         if os.path.exists(FileStorage.__file_path) is True:
             with open(FileStorage.__file_path, 'r') as f:
